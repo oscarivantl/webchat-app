a658bbf8c06d03c73f30c6f0c2e254bb525b56cc
Oscar Ivan Torres Lopez <oscar.torres@unipamplona.edu.co>
2025-02-19 18:30:15 -0500

    first commit

---
+++ webchat-backend/src/main/java/com/quickers/webchatbackend/WebchatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.quickers.webchatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebchatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebchatBackendApplication.class, args);
+	}
+
+}
