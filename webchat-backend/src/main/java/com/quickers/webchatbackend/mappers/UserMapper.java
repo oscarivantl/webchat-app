21aebe5e00319b84563fd73f1de916936e97838d
Oscar Ivan Torres Lopez <oscar.torres@unipamplona.edu.co>
2025-02-22 21:43:49 -0500

    Synchronize the users with Keycloak

---
+++ webchat-backend/src/main/java/com/quickers/webchatbackend/mappers/UserMapper.java
@@ -0,0 +1,35 @@
+package com.quickers.webchatbackend.mappers;
+
+import com.quickers.webchatbackend.entities.User;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+@Service
+public class UserMapper {
+    public User fromTokenAttributes(Map<String, Object> attributes) {
+        User user = new User();
+
+        if (attributes.containsKey("sub")) {
+            user.setId(attributes.get("sub").toString());
+        }
+
+        if (attributes.containsKey("given_name")) {
+            user.setFistName(attributes.get("given_name").toString());
+        } else if (attributes.containsKey("nickname")) {
+            user.setFistName(attributes.get("nickname").toString());
+        }
+
+        if (attributes.containsKey("family_name")) {
+            user.setLastName(attributes.get("family_name").toString());
+        }
+
+        if (attributes.containsKey("email")) {
+            user.setEmail(attributes.get("email").toString());
+        }
+
+        user.setLastSeen(LocalDateTime.now());
+        return user;
+    }
+}
---
+++ webchat-backend/src/main/java/com/quickers/webchatbackend/repositories/UserRepository.java
@@ -0,0 +1,15 @@
+package com.quickers.webchatbackend.repositories;
+
+import com.quickers.webchatbackend.common.UserConstants;
+import com.quickers.webchatbackend.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, String> {
+
+    @Query(name = UserConstants.FIND_USER_BY_EMAIL)
+    Optional<User> findByEmail(@Param("email") String userEmail);
+}
